6aae24d187379ba5f50e549b6d7c524cc2713d54
Rene Moser <mail@renemoser.net>
2021-06-20 09:18:13 +0200

    remove time policy, does not exist

--- scalr/factory/policy.py
+++ scalr/factory/policy.py
@@ -4,7 +4,6 @@ from scalr.model.policy import Policy
 
 from scalr.policy.random import RandomPolicy
 from scalr.policy.web import WebPolicy
-from scalr.policy.time import TimePolicy
 
 
 class PolicyFactory(Factory):
@@ -12,7 +11,6 @@ class PolicyFactory(Factory):
     def __init__(self, config: dict = dict()):
         super().__init__(config=config)
         self.cloud_classes = {
-            'time': TimePolicy,
             'random': RandomPolicy,
             'web': WebPolicy,
         }
