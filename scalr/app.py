53937d719afea112056f3c3be11929c229d51025
Rene Moser <mail@renemoser.net>
2021-06-24 08:25:24 +0200

    func process time_rules

--- scalr/app.py
+++ scalr/app.py
@@ -32,62 +32,70 @@ def get_scaling_factor(policy_configs: list) -> int:
 
     return scaling_factor
 
+def process_time_rules(time_rules: list, base_rule: dict) -> dict:
+    log.info(f"Processing time rules...")
+    for time_rule in time_rules:
+        if 'days_of_year' in time_rule:
+            today = datetime.today().strftime('%b%d')
+            if today not in time_rule['days_of_year']:
+                log.info(f"Skipping days_of_year time rule '{time_rule['name']}'")
+                continue
+            log.debug(f"Today '{today}' in days_of_year of time rule '{time_rule['name']}'")
+
+        if 'weekdays' in time_rule:
+            today = datetime.today().strftime('%a')
+            if today not in time_rule['weekdays']:
+                log.info(f"Skipping time rule '{time_rule['name']}'")
+                continue
+            log.debug(f"Today '{today}' in weekday of time rule '{time_rule['name']}'")
+
+        if 'times_of_day' in time_rule:
+            now = datetime.now().time()
+            for time_range in time_rule['times_of_day']:
+                start, end = time_range.split('-')
+                start_time = datetime.strptime(start, "%H:%M").time()
+                end_time = datetime.strptime(end, "%H:%M").time()
+
+                if start_time > end_time:
+                    start_of_day = datetime.strptime("00:01", "%H:%M").time()
+                    end_of_day = datetime.strptime("23:59", "%H:%M").time()
+
+                    if not (start_time <= now <= end_of_day or start_of_day <= now <= end_time):
+                        log.info(f"Skipping time rule '{time_rule['name']}'")
+                        continue
+                else:
+                    if not (start_time <= now <= end_time):
+                        log.info(f"Skipping time rule '{time_rule['name']}'")
+                        continue
+
+                log.debug(f"{now} in time of day time rule '{time_rule['name']}'")
+                break
+            else:
+                continue
+
+        # Applying rules
+        log.info(f"Applying rule of '{time_rule['name']}'")
+        base_rule.update(**time_rule['rule'])
+
+        # Break on request
+        if time_rule.get('on_match', '') == 'break':
+            log.info(f"Breaking on match requested")
+            return base_rule
+
+    return base_rule
+
+
+
 
 def app() -> None:
     print("")
     try:
         config: dict = read_config(config_source=os.getenv('SCALR_CONFIG', 'config.yml'))
         base_rule = config.get('base_rule') or dict()
-
-        log.info(f"Processing time rules...")
-        for time_rule in config.get('time_rules', []):
-            if 'days_of_year' in time_rule:
-                today = datetime.today().strftime('%b%d')
-                if today not in time_rule['days_of_year']:
-                    log.info(f"Skipping days_of_year time rule '{time_rule['name']}'")
-                    continue
-                log.debug(f"Today '{today}' in days_of_year of time rule '{time_rule['name']}'")
-
-            if 'weekdays' in time_rule:
-                today = datetime.today().strftime('%a')
-                if today not in time_rule['weekdays']:
-                    log.info(f"Skipping time rule '{time_rule['name']}'")
-                    continue
-                log.debug(f"Today '{today}' in weekday of time rule '{time_rule['name']}'")
-
-            if 'times_of_day' in time_rule:
-                now = datetime.now().time()
-                for time_range in time_rule['times_of_day']:
-                    start, end = time_range.split('-')
-                    start_time = datetime.strptime(start, "%H:%M").time()
-                    end_time = datetime.strptime(end, "%H:%M").time()
-
-                    if start_time > end_time:
-                        start_of_day = datetime.strptime("00:01", "%H:%M").time()
-                        end_of_day = datetime.strptime("23:59", "%H:%M").time()
-
-                        if not (start_time <= now <= end_of_day or start_of_day <= now <= end_time):
-                            log.info(f"Skipping time rule '{time_rule['name']}'")
-                            continue
-                    else:
-                        if not (start_time <= now <= end_time):
-                            log.info(f"Skipping time rule '{time_rule['name']}'")
-                            continue
-
-                    log.debug(f"{now} in time of day time rule '{time_rule['name']}'")
-                    break
-                else:
-                    continue
-
-            # Applying rules
-            log.info(f"Applying rule of '{time_rule['name']}'")
-            base_rule.update(**time_rule['rule'])
-
-            # Break on request
-            if time_rule.get('on_match', '') == 'break':
-                log.info(f"Breaking on match requested")
-                break
-
+        base_rule = process_time_rules(
+            time_rules=config.get('time_rules') or list(),
+            base_rule=base_rule
+        )
         if not base_rule.get('enabled', False):
             log.info(f"not enabled, skipping...")
             return
