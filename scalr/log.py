9ddf1ade679c34ec516de15502e03abad718a397
Rene Moser <mail@renemoser.net>
2021-10-20 15:53:31 +0200

    logging fallback base config

--- scalr/log.py
+++ scalr/log.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import logging
 from logging.config import fileConfig
 from dotenv import load_dotenv
@@ -6,7 +8,16 @@ from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-fileConfig('logging.ini')
+logging_config_file_path = os.environ.get('SCALR_LOG_CONFIG', 'logging.ini')
+
+logging_config = Path(logging_config_file_path)
+if logging_config.is_file():
+    fileConfig(logging_config_file_path)
+else:
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=os.environ.get('SCALR_LOG_LEVEL', 'INFO').upper(),
+        format='%(asctime)s - %(name)s:%(levelname)s:%(message)s')
 
 log = logging.getLogger('scalr')
 log.debug('Init')
