d701fe478c70cd6e2e0a67c1fa47de17c9aa8d6b
Rene Moser <mail@renemoser.net>
2022-09-11 11:20:56 +0200

    release 0.12.0

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,6 @@
 import pytest
 from scalr.cloud.factory import CloudAdapterFactory
 from scalr.policy.factory import PolicyAdapterFactory
-from scalr.version import __version__
-
-
-def test_version():
-    assert __version__ == "0.11.0"
 
 
 def test_not_implementend_cloud():
