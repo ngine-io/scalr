cd1b51429a97795fd807d01feb52a4f0d33044a1
Rene Moser <mail@renemoser.net>
2025-06-28 11:38:15 +0200

    bump py version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     install_requires=install_requires,
     tests_require=tests_require,
-    python_requires=">=3.10",
+    python_requires=">=3.12",
     entry_points={
         "console_scripts": [
             "scalr-ngine = scalr.app:main",
